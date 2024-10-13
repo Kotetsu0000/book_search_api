d7abe4f301cb9c7367e18bd431f7470ee67174cd
Kotetsu0000 <yuuiti1234@gmail.com>
2024-10-14 02:13:29 +0900

    chore: Update version to 1.0.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='book_search_api',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     nstall_requires=["requests"],
 )
